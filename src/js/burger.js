3fa885ed7280bad54cf846ceb20c461abac91715
Olexandr Lisovyi <microfox141120@outlook.com>
2023-07-09 17:16:17 +0300

    update adaptive menu

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,7 +1,32 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
+const body = document.querySelector("body");
+const menuLinks = document.querySelectorAll(".menu_link");
+const logo = document.querySelector("#logo");
+const blur = document.querySelector(".blur");
+
+function toggle(elem, selector = "active") {
+  elem.classList.toggle(selector);
+}
+
+function toggleElements() {
+  toggle(burger);
+  toggle(menu);
+  toggle(body, "lock");
+  toggle(blur, "show");
+}
 
 burger.addEventListener("click", function (e) {
-  burger.classList.toggle("active");
-  menu.classList.toggle("active");
+  toggleElements();
+});
+
+document.addEventListener("click", (e) => {
+  let target = e.target;
+  if (menu.classList.contains("active")) {
+    menuLinks.forEach((item) => {
+      if (target === item || target === logo || !target.closest(".header")) {
+        toggleElements();
+      }
+    });
+  }
 });
