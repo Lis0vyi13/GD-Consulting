732a88bc76cbf867e1ef8cd1be0081f43d280db2
Olexandr Lisovyi <microfox141120@outlook.com>
2023-07-21 18:24:25 +0300

    update "requests" tabs

--- src/js/form.js
+++ src/js/form.js
@@ -1,53 +1,20 @@
-const inputRequest = document.querySelector(
-  ".work-together__form-input.relative input"
-);
 const requestOptionsBlock = document.querySelector(
-  ".work-together__form-options"
+  ".work-together__form-input.relative select"
 );
-const tabs = document.querySelectorAll(
-  ".work-together__form-option a.medium_p_font"
-);
-const workTogetherForm = document.querySelector(".work-together__form");
+
 const requestArrow = document.querySelector(".request-arrow");
-const requestSubmitButton = document.querySelector("#requests__submit-button");
-const hiddenInput = document.getElementById("hiddenInput");
 
 function tabsAnimation() {
-  setTimeout(() => {
-    requestOptionsBlock.classList.toggle("tabs-animation");
-    requestArrow.classList.toggle("arrow-animation");
-  }, 10);
-
-  if (requestOptionsBlock.style.overflowY === "auto") {
-    requestOptionsBlock.style.overflowY = "hidden";
-  } else {
-    setTimeout(() => {
-      requestOptionsBlock.style.overflowY = "auto";
-    }, 1000);
-  }
-}
-
-function selectTab(tabText) {
-  hiddenInput.value = tabText;
+  requestArrow.classList.toggle("arrow-animation");
 }
 
 document.addEventListener("click", function (e) {
   if (e.target.closest(".work-together__form-input.relative")) {
-    if (!requestOptionsBlock.classList.contains("show")) {
-      requestOptionsBlock.classList.toggle("show");
-      tabsAnimation();
-    } else {
-      tabsAnimation();
-      setTimeout(() => {
-        requestOptionsBlock.classList.toggle("show");
-      }, 200);
-    }
+    tabsAnimation();
+  } else if (
+    requestArrow.classList.contains("arrow-animation") &&
+    !e.target.closest(".work-together__form-input.relative")
+  ) {
+    tabsAnimation();
   }
-  tabs.forEach(function (tab) {
-    if (e.target === tab) {
-      inputRequest.value = tab.textContent;
-      inputRequest.style.color = "#000";
-      selectTab(tab.textContent);
-    }
-  });
 });
