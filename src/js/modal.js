7bf601e4ee1b0db8efde2b01b0f69139360c4b30
Olexandr Lisovyi <microfox141120@outlook.com>
2023-07-23 01:17:14 +0300

    did disable button when its "sended"

--- src/js/modal.js
+++ src/js/modal.js
@@ -18,9 +18,12 @@ modal.addEventListener("click", function (e) {
 modalSendButton.addEventListener("click", function () {
   if (modalSendButton.classList.contains("_black-button")) {
     modalSendButton.style.setProperty("--top-value", "0px");
+    modalSendButton.disabled = true;
     modalSendButton.classList.remove("_black-button");
     setTimeout(() => {
       modalSendButton.style.setProperty("--top-value", "100px");
+      modalSendButton.disabled = false;
+
       modalSendButton.classList.add("_black-button");
     }, 10000);
   }
