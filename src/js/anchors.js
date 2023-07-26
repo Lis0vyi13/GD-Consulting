f654650e558d8d98822c561989870a660af4be3b
Olexandr Lisovyi <microfox141120@outlook.com>
2023-07-26 17:30:15 +0300

    fix anchors

--- src/js/anchors.js
+++ src/js/anchors.js
@@ -1,14 +1,27 @@
-const satisfiedClientsBlock = document.getElementById("satisfied-clients");
-const heightFromTop = satisfiedClientsBlock.offsetTop;
+const satisfiedClientsBlock = document.querySelector(".satisfied-clients");
 const toSatisfiedClientsElement = document.querySelector("#toSatisfiedClients");
 const toWhatWeDoElement = document.querySelector("#toWhatWeDo");
-const whatWeDoHeight = document.querySelector(".what-we-do").offsetTop;
+const whatWeDo = document.querySelector(".what-we-do");
 
-function moveto(element, value) {
-  element.addEventListener("click", function (e) {
+function moveto(startElement, anchor, ...rest) {
+  startElement.addEventListener("click", function (e) {
+    e.preventDefault();
+    let navHeight = document.querySelector(".nav").clientHeight;
+    let elemRect = anchor.getBoundingClientRect();
+
+    let value = elemRect.top;
+    if (rest.length > 0) {
+      for (const args of rest) {
+        value += args.offsetTop;
+      }
+    }
+    let maxScrollValue = document.documentElement.scrollHeight - window.innerHeight;
+
+    value = Math.min(value + window.scrollY - navHeight - 100, maxScrollValue);
+
     window.scrollTo({ top: value });
   });
 }
 
-moveto(toSatisfiedClientsElement, heightFromTop - 150);
-moveto(toWhatWeDo, whatWeDoHeight - 150);
+moveto(toSatisfiedClientsElement, satisfiedClientsBlock);
+moveto(toWhatWeDoElement, whatWeDo, document.querySelector("#what-we-do__anchor"));
